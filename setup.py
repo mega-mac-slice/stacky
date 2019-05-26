14b350b3a3dd06249f7687077da8c218b100f074
mega-mac-slice <megamacslice@protonmail.com>
2019-05-25 22:46:17 -0600

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stacky',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     url='https://gitlab.com/mega-mac-slice/stacky',
     license='MIT',
