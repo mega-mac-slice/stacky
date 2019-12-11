b6907d51cacb5c57c27eb9f474e569067c897542
mega-mac-slice <megamacslice@protonmail.com>
2019-12-10 23:32:11 -0600

    NA: Add Typing
    Issues: NA

--- stacky/commands.py
+++ stacky/commands.py
@@ -8,12 +8,12 @@ from stacky.config import StackyFile
 logger = logging.getLogger()
 
 
-def _call_command(command) -> (bool, int):
+def _call_command(command: str) -> (bool, int):
     code = subprocess.call(command, shell=True)
     return code == 0, code
 
 
-def _check_output_command(command) -> (bool, int, bytes):
+def _check_output_command(command: str) -> (bool, int, bytes):
     try:
         output = subprocess.check_output(command, shell=True)
         return True, 0, output
@@ -21,7 +21,7 @@ def _check_output_command(command) -> (bool, int, bytes):
         return False, ex.returncode, None
 
 
-def start(stacky_file) -> bool:
+def start(stacky_file: StackyFile) -> bool:
     if not stacky_file.commands or not stacky_file.commands.get("start"):
         return False
 
@@ -34,7 +34,7 @@ def start(stacky_file) -> bool:
     return True
 
 
-def stop(stacky_file) -> bool:
+def stop(stacky_file: StackyFile) -> bool:
     if not stacky_file.commands or not stacky_file.commands.get("stop"):
         return False
 
@@ -47,7 +47,7 @@ def stop(stacky_file) -> bool:
     return True
 
 
-def status(stacky_file: "StackyFile") -> typing.Optional[bytes]:
+def status(stacky_file: StackyFile) -> typing.Optional[bytes]:
     if not stacky_file.commands or not stacky_file.commands.get("status"):
         return None
 
@@ -60,7 +60,7 @@ def status(stacky_file: "StackyFile") -> typing.Optional[bytes]:
     return output
 
 
-def run(stacky_file, command_name) -> typing.Optional[bool]:
+def run(stacky_file: StackyFile, command_name: str) -> typing.Optional[bool]:
     if not stacky_file.commands or not stacky_file.commands.get(command_name):
         return None
 
@@ -74,11 +74,11 @@ def run(stacky_file, command_name) -> typing.Optional[bool]:
     return True
 
 
-def check_status_ok(stacky_file: "StackyFile") -> bool:
+def check_status_ok(stacky_file: StackyFile) -> bool:
     return b"ok" in status(stacky_file)
 
 
-def poll_check_status_ok(stacky_file: "StackyFile", timeout=30) -> bool:
+def poll_check_status_ok(stacky_file: StackyFile, timeout=30) -> bool:
     attempts = 0
     while attempts < timeout:
         logger.debug(f"polling status | {stacky_file.name}\t {attempts}/{timeout}")
